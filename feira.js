dda4ad9d5e8d782b33dd3295c422533b08868174
lucaslabre <48772886+lucaslabre@users.noreply.github.com>
2020-06-23 15:38:53 -0300

    Novas funções - Refrigerantes OK

--- feira.js
+++ feira.js
@@ -1,57 +1,41 @@
-function soma(nome, id, valor){
-	var zero = document.getElementById(id).value;
-	qtd = parseInt(zero) + parseInt(1);           // Incrementa 1 na variável total
-	document.getElementById(id).value = qtd;        // escreve o total do produto vendido
-	document.getElementById("r" + id).value = qtd * parseFloat(valor);  // multiplica a quantidade pelo valor
-	
-	// Para somar o valor total de cada produto 
-	var total = 0;
-	var nom = document.getElementsByName(nome);
-	for (var i = 0; i < nom.length; i++){
-		total += parseFloat(nom[i].value);
-	}
-	document.getElementById("total" + nome).value = total;	
-
-	// Somar no totalzão
-	var totalFinal = 0;
-	var parciais = document.getElementsByName("totalParcial");
-	for (var j = 0; j < parciais.length; j++){
-		totalFinal += parseFloat(parciais[j].value);
-	}
-	document.getElementById("total").value = totalFinal;
-}
-
-function subtrai(nome, id, valor){
-	var zero = document.getElementById(id).value;
-	qtd = parseInt(zero) - parseInt(1);
-	document.getElementById(id).value = qtd;
-	document.getElementById("r" + id).value = qtd * parseFloat(valor);
-
-	// Para somar o valor total de cada produto de cada produto
-	var total = 0;
-	var nom = document.getElementsByName(nome);
-	for (var i = 0; i < nom.length; i++){
-		total += parseFloat(nom[i].value);
-	}
-	document.getElementById("total" + nome).value = total;	
-
-	// Subtrair no totalzão
-	var totalFinal = 0;
-	var parciais = document.getElementsByName("totalParcial");
-	for (var j = 0; j < parciais.length; j++){
-		totalFinal += parseFloat(parciais[j].value);
-	}
-	document.getElementById("total").value = totalFinal;
-}
-
-// Função que soma os refrigerantes
-
-function somaEspecifico(nome, valor){
-	var pega = document.getElementsByName(nome);
-	var alvo = document.getElementById("r" + nome);
+function conta(dep, alvo){
 	var soma = 0;
-	for (var i = 0; i < pega.length; i++){
-		soma += parseFloat(pega[i].value) * parseFloat(valor);
+	var destino = document.getElementById(alvo).value;
+	soma += parseInt(destino) + parseInt(dep.value);
+	document.getElementById(alvo).value = soma;
+}
+
+function soma(dependencia, preco){
+	var valor = 0;
+	var dep = document.getElementById(dependencia).value;
+	valor += parseFloat(dep) * preco;
+	document.getElementById('r' + dependencia).value = valor; 
+}
+
+function somaEspecifico(dependencia, preco){
+	var valor = 0;
+	var dep = document.getElementsByName(dependencia);
+	for (i = 0; i < dep.length; i++){
+		valor += parseFloat(dep[i].value) * parseFloat(preco);
 	}
-	alvo.value = soma;
+	document.getElementById("r" + dependencia).value = valor;
+}
+
+function totalParcial(dependencia){
+	var total = 0;
+	var dep = document.getElementsByName(dependencia);
+	for (var i = 0; i < dep.length; i++){
+		total += parseFloat(dep[i].value);
+	}
+	document.getElementById("total" + dependencia).value = total;
+	totalzao();
+}
+
+function totalzao(){
+	var soma = 0;
+	var dep = document.getElementsByName('totalParcial');
+	for (var i = 0; i < dep.length; i++){
+		soma += parseFloat(dep[i].value);
+	}
+	document.getElementById("total").value = soma;
 }
